1ded069cdf893a83e1fbc242fcc40d9cd845d3f8
Arvel Francisco <76999604+arvelfranciso0@users.noreply.github.com>
2026-01-24 14:31:34 +0800

    Account UI (#23)
    * feat: implement account ui
    
    * feat: add a verification code

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,7 +20,7 @@ export default function Button({
       "bg-primary text-white hover:bg-primary-foreground hover:text-gray-800 shadow-lg shadow-primary/20",
     outline:
       "border border-gray-200 hover:bg-primary hover:border-primary-foreground text-foreground shadow-sm",
-    ghost: "hover:bg-gray-100 hover:text-primary text-muted ",
+    ghost: " hover:text-primary text-muted ",
     warning: "bg-red-800  hover:bg-red-500",
   };
 
