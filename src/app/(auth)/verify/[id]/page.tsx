2bc518a54915f4c2bde1329210b9ba39520afa7d
Arvel Francisco <franciscoarvel123@gmail.com>
2026-02-06 16:08:35 +0800

    feat: verification page

---
+++ src/app/(auth)/verify/[id]/page.tsx
@@ -0,0 +1,32 @@
+import { db } from "@/db";
+import VerificationForm from "../_components/verification";
+import { userVerifications } from "@/db/schema";
+import { and, eq, gt, isNull } from "drizzle-orm";
+import InvalidVerificationPage from "../_components/failed-verification";
+
+export default async function VerificationPage(
+  props: PageProps<"/verify/[id]">,
+) {
+  const { id } = await props.params;
+  const now = new Date();
+  const verification = await db
+    .select({
+      id: userVerifications.id,
+      expireAt: userVerifications.verificationExpiresAt,
+    })
+    .from(userVerifications)
+    .where(
+      and(
+        eq(userVerifications.id, id),
+        gt(userVerifications.verificationExpiresAt, now),
+        isNull(userVerifications.lastUsedAt),
+        isNull(userVerifications.deletedAt),
+      ),
+    )
+    .limit(1);
+
+  if ((!verification || verification.length) === 0) {
+    return <InvalidVerificationPage />;
+  }
+  return <VerificationForm tokenId={id} />;
+}
--- src/repository/access_token.ts
+++ src/repository/access_token.ts
@@ -1,7 +1,7 @@
 import { db } from "@/db";
-import { accessToken } from "@/db/schema";
+import { accessToken, userVerifications } from "@/db/schema";
 import { accessTokenInterface } from "@/interface/data-interface";
-import { eq } from "drizzle-orm";
+import { and, eq, gt, isNull } from "drizzle-orm";
 
 export async function saveAccessToken(data: accessTokenInterface) {
   await db.insert(accessToken).values(data);
@@ -11,7 +11,12 @@ export async function findHashTokenByToken(hashSessionToken: string) {
   const [access_token] = await db
     .select()
     .from(accessToken)
-    .where(eq(accessToken.token, hashSessionToken))
+    .where(
+      and(
+        eq(accessToken.token, hashSessionToken),
+        isNull(accessToken.deletedAt),
+      ),
+    )
     .limit(1);
 
   return access_token;
@@ -20,13 +25,71 @@ export async function findHashTokenByToken(hashSessionToken: string) {
 export async function deleteAccessTokenByHashToken(hashSessionToken: string) {
   const [access_token] = await db
     .delete(accessToken)
-    .where(eq(accessToken.token, hashSessionToken))
+    .where(
+      and(
+        eq(accessToken.token, hashSessionToken),
+        isNull(accessToken.deletedAt),
+      ),
+    )
     .limit(1);
 
   return access_token;
+}
+
+export async function findUserAccessTokenByEmailAndHashVerificationCode(
+  email: string,
+  hashVerificationCode: string,
+) {
+  const now = new Date();
+  const [verification] = await db
+    .select()
+    .from(userVerifications)
+    .where(
+      and(
+        eq(userVerifications.email, email),
+        eq(userVerifications.hashVerificationCode, hashVerificationCode),
+        gt(userVerifications.verificationExpiresAt, now),
+        isNull(userVerifications.deletedAt),
+      ),
+    )
+    .limit(1);
+
+  return verification;
+}
+
+export async function findUserAccessTokenById(id: string) {
+  const now = new Date();
+  const [verification] = await db
+    .select({
+      id: userVerifications.id,
+      hashCode: userVerifications.hashVerificationCode,
+      email: userVerifications.email,
+      salt: userVerifications.salt,
+    })
+    .from(userVerifications)
+    .where(
+      and(
+        eq(userVerifications.id, id),
+        gt(userVerifications.verificationExpiresAt, now),
+        isNull(userVerifications.deletedAt),
+      ),
+    )
+    .limit(1);
+
+  return verification;
 }
 
 export default {
   saveAccessToken,
   findHashTokenByToken,
 };
+
+export async function setAccessTokenLastUseActive(id: string) {
+  const now = new Date();
+  return await db
+    .update(userVerifications)
+    .set({ lastUsedAt: now })
+    .where(
+      and(eq(userVerifications.id, id), isNull(userVerifications.deletedAt)),
+    );
+}
